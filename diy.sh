e97302d0bc57488d1935938fd80a3f66c74ed359
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-04-07 20:42:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ author_list="myself"
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
 
-scripts_base_url_1=https://gitee.com/jaydiao521/scripts/raw/master/
+scripts_base_url_1=https://ghproxy.com/https://raw.githubusercontent.com/jaydiao521/diyjd/main/
 
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.sevencdn.com/whyour/hundun/master/quanx/
 
