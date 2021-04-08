268fa9ff473463406c7efa7a98c7bdb8cd6e188a
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-04-08 13:37:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -68,6 +68,8 @@ done
 
 ##############################  删  除  失  效  的  活  动  脚  本  ##############################
 ## 删除旧版本失效的活动示例： rm -rf ${ScriptsDir}/jd_test.js
-rm -rf ${ScriptsDir}/jd_getfanslove.js
-rm -rf ${ScriptsDir}/jd_fanslove.js
-rm -rf ${ScriptsDir}/jd_shop_add_to_car.js
+rm -rf ${ScriptsDir}/jd_jdaxc.js
+rm -rf ${ScriptsDir}/jd_xxl_gh.js
+rm -rf ${ScriptsDir}/jd_factory_component.js
+rm -rf ${ScriptsDir}/jd_wish.js
+rm -rf ${ScriptsDir}/jd_mgold.js
